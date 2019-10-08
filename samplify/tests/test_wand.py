f73418a3f4ce8b3eeeedd5457774b20733b67313
RoscoeTheDog <54086262+RoscoeTheDog@users.noreply.github.com>
2019-10-07 21:57:01 -0700

    Struct Log added
    Custom Processors added.
    Also organized some of the repo space.

--- samplify/tests/test_wand.py
+++
@@ -1,8 +0,0 @@
-from wand.image import Image
-
-input = 'C:\\Users\\Aspen\\Desktop\\New folder (2)\\835.psd'
-
-with Image(filename=input) as original:
-
-    with original.convert(format='png') as copy:
-        copy.save(filename='C:\\Users\\Aspen\\Desktop\\pictest.png'.format('png'))
